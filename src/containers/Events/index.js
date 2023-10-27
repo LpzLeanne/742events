e246af3b6e5093a0f7f9632027775c9e495262fd
LpzLeanne <130980145+LpzLeanne@users.noreply.github.com>
2023-10-27 15:46:17 +0200

    Fixed event list

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { useData } from "../../contexts/DataContext";
 import EventCard from "../../components/EventCard";
 import Select from "../../components/Select";
-import { useData } from "../../contexts/DataContext";
 import Modal from "../Modal";
 import ModalEvent from "../ModalEvent";
 
@@ -11,27 +11,32 @@ const PER_PAGE = 9;
 
 const EventList = () => {
   const { data, error } = useData();
-  const [type, setType] = useState();
+  const [type, setType] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+
+  // Filter events by category
+  const allEvents = data?.events || [];
+  const filteredEvents = allEvents.filter(
+    (event) => !type || event.type === type
+  );
+
+  // Sort events by date
+  filteredEvents.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  // Apply pagination
+  const startIndex = (currentPage - 1) * PER_PAGE;
+  const endIndex = currentPage * PER_PAGE;
+  const eventsToDisplay = filteredEvents.slice(startIndex, endIndex);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
+  const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
+
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
@@ -45,7 +50,7 @@ const EventList = () => {
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
           <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
+            {eventsToDisplay.map((event) => (
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
                   <EventCard
@@ -61,8 +66,15 @@ const EventList = () => {
           </div>
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+              <a // eslint-disable-next-line react/no-array-index-key
+                key={n}
+                href="#events"
+                onClick={() => setCurrentPage(n + 1)}
+                style={{
+                  fontWeight: n + 1 === currentPage ? "bold" : "normal",
+                  color: n + 1 === currentPage ? "white" : "gray",
+                }}
+              >
                 {n + 1}
               </a>
             ))}
