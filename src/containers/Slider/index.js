5080e39129bcb4d7f78c04d75528470b8f90d253
LpzLeanne <130980145+LpzLeanne@users.noreply.github.com>
2023-11-27 00:20:35 +0100

    Adding test for Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,55 +7,72 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
-  ) || [];
-  
+  const byDateDesc =
+    data?.focus?.sort((evtA, evtB) =>
+      new Date(evtA.date) > new Date(evtB.date) ? 1 : -1
+    ) || [];
+
+  const handleRadioChange = (radioIdx) => {
+    setIndex(radioIdx);
+  };
+
   useEffect(() => {
     const nextCard = () => {
-      setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
+      setIndex((prevIndex) =>
+        prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
+      );
     };
 
-    const interval = setInterval(nextCard, 5000);
+  const interval = setInterval(() => {
+    setIndex((prevIndex) => {
+      nextCard();
+      return prevIndex;
+    });
+  }, 5000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [byDateDesc]);
+  return () => {
+    clearInterval(interval);
+  };
+}, [byDateDesc]);
+
 
   return (
-    <div className="SlideCardList">
+    <div className="SlideCardList" data-testid="slide-card-list">
       {byDateDesc?.map((focus, idx) => (
-        <>
-          <div
-            key={focus.id}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={focus.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{focus.title}</h3>
-                <p>{focus.description}</p>
-                <div>{getMonth(new Date(focus.date))}</div>
-              </div>
+        <div
+          key={focus.id}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={focus.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{focus.title}</h3>
+              <p>{focus.description}</p>
+              <div>{getMonth(new Date(focus.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={_.id}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div
+        className="SlideCard__paginationContainer"
+        data-testid="pagination-container"
+      >
+        <div className="SlideCard__pagination" data-testid="pagination">
+          {byDateDesc.map((focus, radioIdx) => (
+            <input
+              key={focus.id}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => handleRadioChange(radioIdx)}
+              value={radioIdx}
+              data-testid="radio-button"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -1,10 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Slider from "./index";
 import { api, DataProvider } from "../../contexts/DataContext";
 
 const data = {
   focus: [
     {
+      id: 1,
       title: "World economic forum",
       description:
         "Oeuvre à la coopération entre le secteur public et le privé.",
@@ -12,12 +13,14 @@ const data = {
       cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
     {
+      id: 2,
       title: "World Gaming Day",
       description: "Evenement mondial autour du gaming",
       date: "2022-03-29T20:28:45.744Z",
       cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
     {
+      id: 3,
       title: "World Farming Day",
       description: "Evenement mondial autour de la ferme",
       date: "2022-01-29T20:28:45.744Z",
@@ -40,5 +43,126 @@ describe("When slider is created", () => {
     await screen.findByText(
       "Oeuvre à la coopération entre le secteur public et le privé."
     );
+  });
+});
+
+describe("When all the slides are created", () => {
+  it("creates radio buttons", async () => {
+    window.console.error = jest.fn();
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+
+    // Check if the slider render all of data. If expect is false so there is a problem.
+    await waitFor(() => {
+      const slides = screen
+        .getAllByTestId("slide-card-list")[0]
+        .getElementsByClassName("SlideCard");
+      expect(slides.length).toBe(data.focus.length);
+
+      // Check if the radio-button are created according the number of data/slides. If expect is false so there is a problem.
+      const radioButtons = screen.getAllByTestId("radio-button");
+      expect(radioButtons.length).toBe(data.focus.length);
+      expect(radioButtons.length).toBe(slides.length);
+    });
+  });
+});
+
+describe("When all the slides are created", () => {
+  it("they are displayed according their date", async () => {
+    window.console.error = jest.fn();
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+
+    // Check if the slides are displayed in the correct order : from most old to most recent date
+    for (let i = 0; i < data.focus.length - 1; i += 1) {
+      const currentDate = new Date(data.focus[i].date).getTime();
+      const nextDate = new Date(data.focus[i + 1].date).getTime();
+      expect(currentDate).toBeLessThanOrEqual(nextDate);
+    }
+  });
+});
+
+describe("When the slider is displayed", () => {
+  beforeEach(() => {
+    window.console.error = jest.fn();
+    jest.useFakeTimers();
+    api.loadData = jest.fn().mockReturnValue(data);
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  it("automatically transitions to the next slide after 5000ms interval", async () => {
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+
+    // Wait for the initial slide to be rendered
+    await screen.findByText("World economic forum");
+
+    // Find the initial index (0)
+    const initialIndex = screen
+      .getAllByTestId("radio-button")
+      .findIndex((radio) => radio.getAttribute("checked"));
+
+    // Advance the timers by the interval time
+    jest.advanceTimersByTime(5000);
+
+    // Run only the pending timers to ensure that the timers are executed
+    jest.runOnlyPendingTimers();
+
+    // Check if the index has changed after the interval
+    const newIndex = screen
+      .getAllByTestId("radio-button")
+      .findIndex((radio) => radio.checked);
+
+    // Check if the new index is not equal to the initial index (0)
+    expect(newIndex).not.toBe(0);
+    expect(newIndex).not.toBe(initialIndex);
+
+    // Expect that the new index is 1 according to the click on the radio-button 1
+    expect(newIndex).toBe(1);
+  });
+
+  it("changes slides when clicking on a radio button", async () => {
+    render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+
+    // Wait for the first slide to be rendered
+    await screen.findByText("World economic forum");
+
+    // Find the initial index (0)
+    const initialIndex = screen
+      .getAllByTestId("radio-button")
+      .findIndex((radio) => radio.getAttribute("checked"));
+
+    // Click on the second radio button
+    fireEvent.click(screen.getAllByTestId("radio-button")[1]);
+
+    // Check if the index has changed after clicking on the radio button
+    const newIndex = screen
+      .getAllByTestId("radio-button")
+      .findIndex((radio) => radio.checked);
+
+    // Check if the new index is not equal to the initial index (0)
+    expect(newIndex).not.toBe(0);
+    expect(newIndex).not.toBe(initialIndex);
+
+    // Expect that the new index is 1 according to the click on the radio-button 1
+    expect(newIndex).toBe(1);
   });
 });
